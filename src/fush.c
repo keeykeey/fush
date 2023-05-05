e169eb3f4e48069b4bb126734229e1e864a28a0d
keeykeey <sh1n_fuj1to@icloud.com>
2023-05-05 23:58:03 +0900

    [update]入力を出力するだけのshell

--- src/fush.c
+++ src/fush.c
@@ -16,27 +16,29 @@ command_t parse_command(char command[COMMAND_LINE_MAX_LEN])
     return cmdt;
 }
 
-void exec_command(command_t command)
+int exec_command(command_t command)
 {
-    int i = 0;
-    while(command.cline[i]) {
-        putchar(command.cline[i++]);
+    if (strcmp(command.cline, "stop") == 0) {
+        return 0;
+    } else {
+        int i = 0;
+        while(command.cline[i]) {
+            putchar(command.cline[i++]);
+        }
+        putchar('\n');
+        return 1;
     }
-    putchar('\n');
 }
 
 int main(void) {
-    while(1) {
+    int wait = 1;
+    while (wait) {
         char c[30] = {};
         catchCommandInput(c);
 
         command_t cmdt = parse_command(c);
 
-        if (strcmp(cmdt.cline,"stop") == 0) {
-            break;
-        } else {
-            exec_command(cmdt);
-        }
+        wait = exec_command(cmdt);
     }
     return 0;
 }
